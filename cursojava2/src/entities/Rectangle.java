d77e9328dfae1abef1f0466beecce65077818e63
eduardomsj <eduardomsj@gmail.com>
2018-12-02 10:07:29 -0200

    Exercicio 1 Capitulo 3

---
+++ cursojava2/src/entities/Rectangle.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class Rectangle {
+
+	public double widht;
+	public double height;
+	
+	public double area() {
+		return widht * height;
+	}
+	
+	public double perimeter() {
+		return 2 * (widht + height);
+	}
+	
+	public double diagonal() {
+		return Math.sqrt(widht * widht + height * height);
+	}
+	
+	public String toString() {
+		return "AREA = "
+				+ area()				
+				+ " PERIMETER = "
+				+ perimeter()
+				+ " DIAGONAL =  "
+				+ diagonal();
+	}
+}
